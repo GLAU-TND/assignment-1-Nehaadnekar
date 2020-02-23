2dd29df61f45d1037f36146ee45ed31f53712f4a
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-23 07:28:40 -0800

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ class Main
   {
     System.out.println("Welcome to DBC's Contact List App");
     System.out.println("Press 1 to add a new contact");
-    System.out.println(Press 2 to view all contacts);
+    System.out.println("Press 2 to view all contacts");
     System.out.println("Press 3 to search for a contact");
     System.out.println("Press 4 to delete a contact);
     System.out.println("Press 5 to exit program");
