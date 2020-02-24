46316716991e9536dbbf2967e6ac03626f0ac8bd
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-24 08:01:02 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,43 @@
 import java.util.Scanner;
-class Main
+
+public class Main
 {
-  public static void main(String args[])
-  {
-    
-    System.out.println("Welcome to Neha's Contact List App");
-    System.out.println("Press 1 to add a new contact");
-    System.out.println("Press 2 to view all contacts");
-    System.out.println("Press 3 to search for a contact");
-    System.out.println("Press 4 to delete a contact);
-    System.out.println("Press 5 to exit program");
-    Scanner sc= new Scanner(System.in);
-    Menu obj=new Menu();
-    int ch=sc.nextInt();
-    switch(ch)
-   {
-     case 1: obj.addCon();
-             break;
-     case 2: obj.showCon();
-             break;
-     case 3: obj.searchCon();
-             break;
-     case 4: obj.deleteCon();
-             break;
-     case 5: obj.exit();
-             break;
+    public static void main(String[] args) 
+    {
+	while(true)
+        {
+        System.out.println("Welcome to Neha's Contact List App");
+        System.out.println("Press 1 to add a new contact");
+        System.out.println("Press 2 to view all contacts");
+        System.out.println("Press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit program ");
+        Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	Contacts obj=new Contacts();
+	if(n==1)
+	{
+   		 obj.addcontact();
+	}
+	else if(n==2)
+	{
+    		obj.view();
+
+	}
+	else if(n==3)
+	{
+  		  obj.search();
+
+	}
+	else if(n==4)
+	{
+   		 obj.delete();
+	}
+	else if (n==5)
+	{
+ 		System.out.println("thankyou ,visit again !");  
+		break;
+	}
+	}
     }
-	
-                       
-                  
-    
-   }
- }
+}
