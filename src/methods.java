fd2c4ceea9696507bbdd4a763bf51ab26d9d0b7c
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-24 08:27:55 -0800

    Create methods.java

---
+++ src/methods.java
@@ -0,0 +1,9 @@
+package adt;
+public interface detailOfFunction
+{
+    public  void add();
+    public void view();
+    public void search();
+    public void delete();
+    public void exit();
+}
