65d0df8cda9d93a2251d5b2e14f1e3d6747ca85d
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-24 08:51:15 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,8 @@
-import java.util.Scanner;
+package adt;
+public class Main {
+    public static void main(String[] args) {
 
-public class Main
-{
-    public static void main(String[] args) 
-    {
-	while(true)
-        {
-        System.out.println("Welcome to Neha's Contact List App");
-        System.out.println("Press 1 to add a new contact");
-        System.out.println("Press 2 to view all contacts");
-        System.out.println("Press 3 to search for a contact");
-        System.out.println("Press 4 to delete a contact");
-        System.out.println("Press 5 to exit program ");
-        Scanner sc=new Scanner(System.in);
-	int num=sc.nextInt();
-	Contact obj=new Contact();
-	if(n==1)
-	{
-   		 obj.addContact();
-	}
-	else if(n==2)
-	{
-    		obj.view();
-
-	}
-	else if(n==3)
-	{
-  		  obj.search();
-
-	}
-	else if(n==4)
-	{
-   		 obj.delete();
-	}
-	else if (n==5)
-	{
- 		System.out.println("thankyou ,visit again !");  
-		break;
-	}
-	}
+        users call=new users();
+        call.contactlist();
     }
 }
