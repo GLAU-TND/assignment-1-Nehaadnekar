39dc06ad157c57c39acfeff5cb0065fe4fddda95
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-24 08:52:13 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package adt;
 public class Main {
     public static void main(String[] args) {
 
-        users call=new users();
-        call.contactlist();
+        Users call=new Users();
+        call.contactList();
     }
 }
