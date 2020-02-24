3bba26a7572ccb63839abaaee63a358036ee737a
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-02-24 07:45:18 -0800

    Update contact_methods.java

--- src/contact_methods.java
+++ src/contact_methods.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Contact {
-    public static ArrayList<Person> contact=new ArrayList<>();
+public class Contact 
+{
+public static ArrayList<Person> contact=new ArrayList<>();
 
-public void addContact(){
+public void addContact()
+{
     Scanner sc=new Scanner(System.in);
-    System.out.println("add a new contact: \n" +
-            "Please enter the first Name:");
+    System.out.println("add a new contact: \n" + "Please enter the first Name:");
     String firstname=sc.nextLine();
     System.out.println("Last Name");
     String lastname=sc.nextLine();
@@ -18,8 +19,8 @@ public void addContact(){
     list.add(num);
     System.out.println("Would you like to add another contact number? (y/n)");
     String num2=sc.nextLine();
-
-    while(n.equals("y")){
+    while(n.equals("y"))
+    {
         num=sc.nextLine();
         list.add(num);
         System.out.println("Would you like to add another contact number? (y/n)");
@@ -28,37 +29,43 @@ num2=sc.nextLine();
     System.out.println("Would you like to add email address? (y/n): ");
     num2=sc.nextLine();
     String mailID;
-    if (num.equals("y")){
+    if (num.equals("y"))
+    {
         mailID=sc.nextLine();
     }
-    else{
+    else
+    {
         mailID=null;
     }
-
-
-contact.add(new Person(firstname,lastname,list,mailID));
+    contact.add(new Person(firstname,lastname,list,mailID));
     System.out.println(contact);
     Collections.sort(contact,new Sortbyname());
-
-
 }
-void view(){
-    for(int i=0;i<contact.size();i++){
-    System.out.println(contact.get(i));}
+void view()
+{
+    for(int i=0;i<contact.size();i++)
+    {
+    System.out.println(contact.get(i));
+    }
 }
-void search(){
+void search()
+{
     System.out.println("search for a contact from their first names: ");
     Scanner sc=new Scanner(System.in);
     String ans=sc.nextLine();
-    for(int i=0;i<contact.size();i++){
-        if(Person.equalsto(contact.get(i),ans)){
+    for(int i=0;i<contact.size();i++)
+    {
+        if(Person.equalsto(contact.get(i),ans))
+        {
             System.out.println(contact.get(i));
 
         }
     }
-}void delete(){
+}void delete()
+{
         System.out.println("Here are all your contacts: ");
-        for(int i=0;i<contact.size();i++){
+        for(int i=0;i<contact.size();i++)
+        {
             System.out.println((i+1) +" . " + Person.getFirstname(contact.get(i)));
         }
         System.out.println("Press the number against the contact to delete it:");
@@ -66,5 +73,5 @@ void search(){
         int i=sc.nextInt();
         System.out.println(Person.getFirstname(contact.get(i))+" contact deleted from list!");
         contact.remove(i);
-
-    }
+}
+}
